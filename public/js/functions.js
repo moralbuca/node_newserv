5c8d5b38b87fa77565175acf694fe82b28eb59ab
moralbuca <142737194+moralbuca@users.noreply.github.com>
2023-08-20 15:55:06 -0500

    Update functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,7 +1,7 @@
 /**
  * CONFIGURACION 
  */
-    const API_URL = 'http://127.0.0.1:8080'; // Cambiar según convenga.
+    const API_URL = 'https://app-d746c3bb-5c2f-48f9-bdc4-04db663ee8bc.cleverapps.io/'; // Cambiar según convenga.
     // const API_URL = 'https://apinewentrega.cleverapps.io'; // Cambiar según convenga.
 
 
